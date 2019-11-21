71fbc298f44b2cac5a31f13e9f29744d3867f432
Yagnik27 <yagnikpansuriya27@gmail.com>
2019-11-21 11:54:08 +0530

    updated

--- GateScorer/home/models.py
+++ GateScorer/home/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 
 class CPerson(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
-    #def __str__(self):
-     #   return self.user.first_name+" "+self.user.last_name
+    def __str__(self):
+      return self.user.first_name+" "+self.user.last_name
     
